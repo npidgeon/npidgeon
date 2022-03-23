3b3a3f0dd17a443d976ebbe8c4ca1a8459722100
npidgeon <102199106+npidgeon@users.noreply.github.com>
2022-03-22 22:25:31 -0400

    Update README.js

--- README.js
+++ README.js
@@ -6,7 +6,7 @@ console.log(`- 👋 Hi, I’m ${username}`);
 
 console.log("- 👀 I’m interested in cloud computing, AWS, and programming languages that are primarily used in the AWS infrastructure. I've been self-studying through platforms like freecodecamp, Codecademy, The Odin Project, and Pluralsight to build a strong foundation, and may be pursuing an online MSCS in the near future.");
 
-console.log("- 📖 I'm currently pursuing IT certifications, namely Network+ and CCNA to build a foundation in networking before moving on to AWS-specific certs like Cloud Practicioner and Cloud Developer.");
+console.log("- 📖 I'm currently pursuing related certifications, namely Network+ and CCNA to build a foundation in networking before moving on to AWS-specific certs like Cloud Practicioner and Cloud Developer.");
 
 console.log("- 🌱 I’m currently learning Java, JavaScript, Python, C++, and Go.");
 
