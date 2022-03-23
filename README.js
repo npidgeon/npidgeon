6d646f18e8d80e03a75cef571c82ca3c3ab10065
Nick Pidgeon <102199106+npidgeon@users.noreply.github.com>
2022-03-22 23:37:50 -0400

    Update README.js

--- README.js
+++ README.js
@@ -4,7 +4,7 @@ let email = "npidgeon97@gmail.com";
 
 console.log(`- 👋 Hi, I’m ${username}`);
 
-console.log("- 👀 I’m interested in cloud computing, AWS, and programming languages that are primarily used in the AWS infrastructure. I've been self-studying through platforms like freecodecamp, Codecademy, The Odin Project, and Pluralsight to build a strong foundation, and may be pursuing an online MSCS in the near future.");
+console.log("- 👀 I’m interested in cloud computing, AWS, and programming languages that are primarily used in the AWS infrastructure. I've been self-studying through platforms like freecodecamp, The Odin Project, and Pluralsight to build a strong foundation. I'm also utilizing textbooks like Sweigart's Automate the Boring Stuff with Python. I may be pursuing an online MSCS in the near future.");
 
 console.log("- 📖 I'm currently pursuing related certifications, namely Network+ and CCNA to build a foundation in networking before moving on to AWS-specific certs like Cloud Practicioner and Cloud Developer.");
 
