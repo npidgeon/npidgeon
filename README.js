0b3f49c1cde753fc50b811f4e8fcd61c5d5ffb9e
npidgeon <102199106+npidgeon@users.noreply.github.com>
2022-03-22 22:18:46 -0400

    Update and rename README.md to README.js

---
+++ README.js
@@ -0,0 +1,11 @@
+let username = @npidgeon;
+
+console.log(`- 👋 Hi, I’m ${username}`);
+
+console.log("- 👀 I’m interested in cloud computing, AWS, and programming languages that are primarily used in the AWS infrastructure. I've been self-studying through platforms like freecodecamp, Codecademy, The Odin Project, and Pluralsight to build a strong foundation, and may be pursuing an online MSCS in the near future.");
+
+console.log("- 🌱 I’m currently learning Java, JavaScript, Python, C++, and Go.")
+console.log("- 💞️ I’m looking to collaborate on any intermediate level projects to build my Git portfolio.")
+console.log("- 📫 How to reach me ... npidgeon97 @ gmail . com")
+
+
