e122e51040e951ba443494a1ea968cdd37dd8293
npidgeon <102199106+npidgeon@users.noreply.github.com>
2022-03-22 22:25:08 -0400

    Update README.js

--- README.js
+++ README.js
@@ -1,11 +1,16 @@
-let username = @npidgeon;
+let username = "@npidgeon";
+
+let email = "npidgeon97@gmail.com";
 
 console.log(`- 👋 Hi, I’m ${username}`);
 
 console.log("- 👀 I’m interested in cloud computing, AWS, and programming languages that are primarily used in the AWS infrastructure. I've been self-studying through platforms like freecodecamp, Codecademy, The Odin Project, and Pluralsight to build a strong foundation, and may be pursuing an online MSCS in the near future.");
 
-console.log("- 🌱 I’m currently learning Java, JavaScript, Python, C++, and Go.")
-console.log("- 💞️ I’m looking to collaborate on any intermediate level projects to build my Git portfolio.")
-console.log("- 📫 How to reach me ... npidgeon97 @ gmail . com")
+console.log("- 📖 I'm currently pursuing IT certifications, namely Network+ and CCNA to build a foundation in networking before moving on to AWS-specific certs like Cloud Practicioner and Cloud Developer.");
 
+console.log("- 🌱 I’m currently learning Java, JavaScript, Python, C++, and Go.");
+
+console.log("- 💞️ I’m looking to collaborate on any intermediate level projects to build my Git portfolio.");
+
+console.log(`- 📫 You can reach me at ${email}`)
 
