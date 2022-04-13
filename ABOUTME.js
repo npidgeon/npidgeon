b9bb26880a9296cf71e86fbdf23a93dd3b65a91d
Nick Pidgeon <102199106+npidgeon@users.noreply.github.com>
2022-04-13 17:04:03 -0400

    Update and rename README.js to ABOUTME.js

--- ABOUTME.js
+++ ABOUTME.js
@@ -4,8 +4,7 @@ let email = "npidgeon97@gmail.com";
 
 console.log(`- 👋 Hi, I’m ${username}`);
 
-console.log("- 👀 I’m interested in cloud computing, AWS, and web development. I've been self-studying through platforms like freecodecamp, The Odin Project, and Pluralsight to build a strong foundation. I'm also utilizing textbooks like Sweigart's Automate the Boring Stuff with Python and MOOCs like Harvard's CS50. I may be pursuing an online MSCS in the near future.");
-
+console.log("- 👀 I’m interested in cloud computing, AWS, and web development. I've been self-studying through platforms like Full Stack Open, CS50, and various CS textbooks to build a strong foundation.  I may pursue an online MSCS in the near future.");
 
 console.log("- 🌱 I'm focusing primarily on ES6/JavaScript, but would like to be competent in several langauges as a full-stack web developer.");
 
